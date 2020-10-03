f0b0174b9b2e9413bd4c5d2ed90de4f18d932ba1
Lindsay Esterman <lindsayesterman@gmail.com>
2020-10-03 12:59:19 -0400

    refactoring

--- src/app.js
+++ src/app.js
@@ -13,36 +13,41 @@ app.use(cors())
 app.use(helmet())
 
 app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
     .then(articles => {
-      res.json(articles)
+        res.json(articles)
     })
     .catch(next)
 })
 
 app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
     .then(article => {
-      res.json(article)
+        if (!article) {
+            return res.status(404).json({
+                error: { message: `Article doesn't exist` }
+            })
+        }
+        res.json(article)
     })
     .catch(next)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+    res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: 'Server error' }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: 'Server error' }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
 })
 
 module.exports = app
